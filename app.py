dbcadb61ebb12e7ea1c568539e9c9339cf3842ad
Alexey Kulikov <alexeyqu@gmail.com>
2020-03-28 21:59:07 +0000

    simple session flow

--- app.py
+++ app.py
@@ -21,7 +21,15 @@ logger = logging.getLogger(__name__)
 class TicketToRide:
     def __init__(self, token):
         self.updater = Updater(token, use_context=True)
-        self.init_game()
+        self.session = {'users': [], 'turn': None, 'open': True}
+
+    def join(self, update, context):
+        if self.session['open']:
+            self.session['users'].append(update.message.chat_id)
+            logger.info('Added {} to the game'.format(update.message.chat_id))
+            update.message.reply_text('You\'ve been added to the session')
+        else:
+            update.message.reply_text('Sorry, the session is closed already')
 
     def init_game(self):
         self.deck = Deck()
@@ -29,9 +37,14 @@ class TicketToRide:
         self.state = [self.deck.draw() for _ in range(5)]
 
     def start(self, update, context):
-        update.message.reply_text('Hi! Ticket To Ride game is being initialized...')
+        if not self.session['open'] or update.message.chat_id not in self.session['users']:
+            update.message.reply_text('Sorry, you have to /join the open session first')
+            return
+        update.message.reply_text('Hi! This Ticket To Ride game is being initialized...')
+        logger.info('Started new game')
         self.init_game()
         self.show_state(update, context)
+        self.session['open'] = False
 
     def show_state(self, update, context):
         update.message.reply_text('''
@@ -50,6 +63,7 @@ class TicketToRide:
     def main(self):
         """Start the bot."""
         dispatcher = self.updater.dispatcher
+        dispatcher.add_handler(CommandHandler("join", self.join))
         dispatcher.add_handler(CommandHandler("start", self.start))
         # dispatcher.add_handler(CommandHandler("random", self.random))
         # dispatcher.add_handler(CommandHandler("1", self.start))
